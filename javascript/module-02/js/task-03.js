2004302d49ffd4bf914de37b925fd433cccf8a53
Dmitrij Shamota <su161084sd@gmail.com>
2020-07-17 01:02:00 +0300

    finish  js-module-02-task4

--- javascript/module-02/js/task-03.js
+++ javascript/module-02/js/task-03.js
@@ -13,4 +13,8 @@ const findLongestWord = function (string) {
 
 buttonRef.addEventListener('click', () => {
   console.log(findLongestWord('The quick brown fox jumped over the lazy dog'));
+
+  console.log(findLongestWord('Google do a roll'));
+
+  console.log(findLongestWord('May the force be with you'));
 });
--- javascript/module-02/js/task-04.js
+++ javascript/module-02/js/task-04.js
@@ -1,2 +1,20 @@
 const buttonRef = document.querySelector('#task_04');
-buttonRef.addEventListener('click', () => {});
+const formatString = function (string) {
+  return string.length <= 40 ? string : `${string.slice(0, 41)}...`;
+};
+
+buttonRef.addEventListener('click', () => {
+  console.log(formatString('Curabitur ligula sapien, tincidunt non.'));
+
+  console.log(
+    formatString('Vestibulum facilisis, purus nec pulvinar iaculis.'),
+  );
+
+  console.log(formatString('Curabitur ligula sapien.'));
+
+  console.log(
+    formatString(
+      'Nunc sed turpis. Curabitur a felis in nunc fringilla tristique.',
+    ),
+  );
+});

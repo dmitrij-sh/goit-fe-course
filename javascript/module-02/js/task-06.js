ca303f27e7f7479f37ce89e5e05028dfbaaaa04e
Dmitrij Shamota <su161084sd@gmail.com>
2020-07-18 12:06:35 +0300

    finish  js-module-02-task6

--- javascript/module-02/js/task-06.js
+++ javascript/module-02/js/task-06.js
@@ -1,2 +1,25 @@
 const buttonRef = document.querySelector('#task_06');
-buttonRef.addEventListener('click', () => {});
+buttonRef.addEventListener('click', () => {
+  let input;
+  const numbers = [];
+  let total = 0;
+
+  do {
+    input = prompt('Веедите число');
+
+    if (input !== null) {
+      if (Number.isNaN(+input)) {
+        alert('Было введено не число, попробуйте еще раз');
+        continue;
+      }
+      numbers.push(+input);
+    }
+  } while (input !== null);
+
+  if (numbers.length > 0) {
+    for (let number of numbers) {
+      total += +number;
+    }
+    console.log(`Общая сумма чисел равна ${total}`);
+  }
+});

739b17c7dc800bc4f9a46cf91752580c622d37ae
Dmitrij Shamota <su161084sd@gmail.com>
2020-07-19 13:55:00 +0300

    finish js-module-03-task1

--- javascript/module-03/js/task-01.js
+++ javascript/module-03/js/task-01.js
@@ -1,3 +1,18 @@
 const buttonRef = document.querySelector('#task_01');
+const user = {
+  name: 'Mango',
+  age: 20,
+  hobby: 'html',
+  premium: true,
+};
 
-buttonRef.addEventListener('click', () => {});
+user.mood = 'happy';
+user.hobby = 'skydiving';
+user.premium = false;
+const keys = Object.keys(user);
+
+buttonRef.addEventListener('click', () => {
+  for (const key of keys) {
+    console.log(`${key}: ${user[key]}`);
+  }
+});

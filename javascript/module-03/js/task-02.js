eaa7aa8bd422e5f23bdb588a5c130130ceeffc0a
Dmitrij Shamota <su161084sd@gmail.com>
2020-07-19 20:02:26 +0300

    finish js-module-03-task3

--- javascript/module-03/js/task-02.js
+++ javascript/module-03/js/task-02.js
@@ -1,3 +1,14 @@
 const buttonRef = document.querySelector('#task_02');
+const countProps = function (obj) {
+  return Object.keys(obj).length;
+};
 
-buttonRef.addEventListener('click', () => {});
+buttonRef.addEventListener('click', () => {
+  console.log(countProps({}));
+
+  console.log(countProps({ name: 'Mango', age: 2 }));
+
+  console.log(
+    countProps({ mail: 'poly@mail.com', isOnline: true, score: 500 }),
+  );
+});
--- javascript/module-03/js/task-03.js
+++ javascript/module-03/js/task-03.js
@@ -1,3 +1,21 @@
 const buttonRef = document.querySelector('#task_03');
+const findBestEmployee = function (employees) {
+  const arrEmployee = Object.entries(employees);
+  let bestEmployee;
+  let bestValue = 0;
+  for (const [key, value] of arrEmployee) {
+    if (value > bestValue) {
+      bestEmployee = key;
+      bestValue = value;
+    }
+  }
+  return `${bestEmployee}: ${bestValue}`;
+};
 
-buttonRef.addEventListener('click', () => {});
+buttonRef.addEventListener('click', () => {
+  console.log(findBestEmployee({ ann: 29, david: 35, helen: 1, lorence: 99 }));
+
+  // console.log(findBestEmployee({ poly: 12, mango: 17, ajax: 4 }));
+
+  // console.log(findBestEmployee({ lux: 147, david: 21, kiwi: 19, chelsy: 38 }));
+});

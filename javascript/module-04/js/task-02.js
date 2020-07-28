329e86ab0283d6c1c955300142709651c5ccf481
Dmitrij Shamota <su161084sd@gmail.com>
2020-07-28 22:57:20 +0300

    finish js-module-04-task2

--- javascript/module-04/js/task-02.js
+++ javascript/module-04/js/task-02.js
@@ -1,3 +1,27 @@
 const buttonRef = document.querySelector('#task_02');
+const inventory = {
+  items: ['Knife', 'Gas mask'],
+  add(itemName) {
+    console.log(`Adding ${itemName} to inventory`);
+    this.items.push(itemName);
+  },
+  remove(itemName) {
+    console.log(`Removing ${itemName} from inventory`);
+    this.items = this.items.filter(item => item !== itemName);
+  },
+};
 
-buttonRef.addEventListener('click', () => {});
+const invokeInventoryAction = function(itemName, action) {
+  console.log(`Invoking action on ${itemName}`);
+  action.bind(inventory)(itemName);
+};
+
+buttonRef.addEventListener('click', () => {
+  invokeInventoryAction('Medkit', inventory.add);
+
+console.log(inventory.items);
+
+invokeInventoryAction('Gas mask', inventory.remove);
+
+console.log(inventory.items);
+});

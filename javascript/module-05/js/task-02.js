72c04a537dd8b473d623ad77282b9b007a174474
Dmitrij Shamota <su161084sd@gmail.com>
2020-08-02 15:57:54 +0300

    finish js-module-05-task2

--- javascript/module-05/js/task-02.js
+++ javascript/module-05/js/task-02.js
@@ -1,3 +1,31 @@
 const buttonRef = document.querySelector('#task_02');
 
-buttonRef.addEventListener('click', () => {});
+class User {
+  constructor({name, age, followers}) {
+    this.name = name;
+    this.age = age;
+    this.followers = followers;
+  }
+
+  getInfo() {
+    console.log(`User ${this.name} is ${this.age} years old and has ${this.followers} followers`);
+  }
+}
+
+buttonRef.addEventListener('click', () => {
+  const mango = new User({
+    name: 'Mango',
+    age: 2,
+    followers: 20,
+  });
+
+  mango.getInfo();
+
+  const poly = new User({
+    name: 'Poly',
+    age: 3,
+    followers: 17,
+  });
+
+  poly.getInfo();
+});

8b2b313239ec58f9e0350dadb4474b3ce318eebe
Dmitrij Shamota <su161084sd@gmail.com>
2020-08-03 00:18:00 +0300

    finish js-module-05-task4

--- javascript/module-05/js/task-04.js
+++ javascript/module-05/js/task-04.js
@@ -1,3 +1,35 @@
 const buttonRef = document.querySelector('#task_04');
+class StringBuilder {
+  constructor (value) {
+    this._value = value;
+  }
 
-buttonRef.addEventListener('click', () => {});
+  get value() {
+    return this._value;
+  }
+
+  append(str) {
+    this._value += str;
+  }
+
+  prepend(str) {
+    this._value = str + this._value;
+  }
+
+  pad(str) {
+    this._value = str + this._value + str;
+  }
+}
+
+buttonRef.addEventListener('click', () => {
+  const builder = new StringBuilder('.');
+
+  builder.append('^');
+  console.log(builder.value);
+
+  builder.prepend('^');
+  console.log(builder.value);
+
+  builder.pad('=');
+  console.log(builder.value);
+});
--- javascript/module-05/js/task-05.js
+++ javascript/module-05/js/task-05.js
@@ -1,3 +1,79 @@
 const buttonRef = document.querySelector('#task_05');
+class Car {
+  /*
+   * Добавь статический метод `getSpecs(car)`,
+   * который принимает объект-машину как параметр и выводит
+   * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
+   */
 
-buttonRef.addEventListener('click', () => {});
+  /*
+   * Конструктор получает объект настроек.
+   *
+   * Добавь свойства будущеего экземпляра класса:
+   *  speed - текущая скорость, изначально 0
+   *  price - цена автомобиля
+   *  maxSpeed - максимальная скорость
+   *  isOn - заведен ли автомобиль, значения true или false. Изначально false
+   *  distance - общий киллометраж, изначально 0
+   */
+  constructor() {}
+
+  /*
+   * Добавь геттер и сеттер для свойства price,
+   * который будет работать с свойством цены автомобиля.
+   */
+
+  /*
+   * Добавь код для того чтобы завести автомобиль
+   * Записывает в свойство isOn значение true
+   */
+  turnOn() {}
+
+  /*
+   * Добавь код для того чтобы заглушить автомобиль
+   * Записывает в свойство isOn значение false,
+   * и сбрасывает текущую скорость в 0
+   */
+  turnOff() {}
+
+  /*
+   * Добавялет к свойству speed полученное значение,
+   * при условии что результирующая скорость
+   * не больше чем значение свойства maxSpeed
+   */
+  accelerate(value) {}
+
+  /*
+   * Отнимает от свойства speed полученное значение,
+   * при условии что результирующая скорость не меньше нуля
+   */
+  decelerate(value) {}
+
+  /*
+   * Добавляет в поле distance киллометраж (hours * speed),
+   * но только в том случае если машина заведена!
+   */
+  drive(hours) {}
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+buttonRef.addEventListener('click', () => {
+  mustang.turnOn();
+  mustang.accelerate(50);
+  mustang.drive(2);
+
+  Car.getSpecs(mustang);
+  // maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+  mustang.decelerate(20);
+  mustang.drive(1);
+  mustang.turnOff();
+
+  Car.getSpecs(mustang);
+  // maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+  console.log(mustang.price); // 2000
+  mustang.price = 4000;
+  console.log(mustang.price); // 4000
+});

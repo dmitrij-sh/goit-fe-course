0d780e9c03a1d3f8bc0bed9c7fcd630324489320
Dmitrij Shamota <su161084sd@gmail.com>
2020-08-02 18:33:14 +0300

    finish js-module-05-task3

--- javascript/module-05/js/task-03.js
+++ javascript/module-05/js/task-03.js
@@ -1,3 +1,39 @@
 const buttonRef = document.querySelector('#task_03');
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
 
-buttonRef.addEventListener('click', () => {});
+  getItems() {
+    return this.items;
+  }
+
+  addItem(item) {
+    this.items.push(item);
+  }
+
+  removeItem(item) {
+    if (this.items.indexOf(item) !== -1) {
+      this.items.splice(this.items.indexOf(item), 1);
+      console.log('good');
+    }
+  }
+}
+
+buttonRef.addEventListener('click', () => {
+  const storage = new Storage([
+    'Нанитоиды',
+    'Пролонгер',
+    'Железные жупи',
+    'Антигравитатор',
+  ]);
+
+  const items = storage.getItems();
+  console.table(items);
+
+  storage.addItem('Дроид');
+  console.table(storage.items);
+
+  storage.removeItem('Пролонгер');
+  console.table(storage.items);
+});

52b12e558d972c09e9a15aa37aebb6915128d6ee
Dmitrij Shamota <su161084sd@gmail.com>
2020-08-09 02:03:14 +0300

    finish js-module-06-task2

--- javascript/module-06/js/task-01.js
+++ javascript/module-06/js/task-01.js
@@ -1,9 +1,7 @@
+import users from "./users.js"
 const buttonRef = document.querySelector('#task_01');
-const getUserNames = users => {
-  // твой код
-};
+const getUserNames = users => users.map((user) => user.name);
 
 buttonRef.addEventListener('click', () => {
   console.log(getUserNames(users));
-// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
 });
--- javascript/module-06/js/task-02.js
+++ javascript/module-06/js/task-02.js
@@ -1,8 +1,7 @@
+import users from "./users.js"
 const buttonRef = document.querySelector('#task_02');
-const getUsersWithEyeColor = (users, color) => {
-  // твой код
-};
+const getUsersWithEyeColor = (users, color) => users.filter((user) => user.eyeColor === color);
 
 buttonRef.addEventListener('click', () => {
-  console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+  console.log(getUsersWithEyeColor(users, 'blue'));
 });

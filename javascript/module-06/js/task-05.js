449a7fbe7812d4aaa1964e1f0b4421d0d240775e
Dmitrij Shamota <su161084sd@gmail.com>
2020-08-09 14:33:18 +0300

    finish js-module-06-task6

--- javascript/module-06/js/task-05.js
+++ javascript/module-06/js/task-05.js
@@ -1,9 +1,9 @@
+import users from  "./users.js"
 const buttonRef = document.querySelector('#task_05');
-const getUserWithEmail = (users, email) => {
-  // твой код
-};
+const getUserWithEmail = (users, email) =>
+users.find((user) => user.email === email);
 
 buttonRef.addEventListener('click', () => {
-  console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
-  console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
+  console.log(getUserWithEmail(users, 'shereeanthony@kog.com'));
+  console.log(getUserWithEmail(users, 'elmahead@omatom.com'));
 });
--- javascript/module-06/js/task-06.js
+++ javascript/module-06/js/task-06.js
@@ -1,11 +1,9 @@
+import users from "./users.js"
 const buttonRef = document.querySelector('#task_06');
-const getUsersWithAge = (users, min, max) => {
-  // твой код
-};
+const getUsersWithAge = (users, min, max) =>
+users.filter((user) => user.age >= min && user.age <= max);
 
 buttonRef.addEventListener('click', () => {
-  console.log(getUsersWithAge(users, 20, 30)); // [объект Ross Vazquez, объект Elma Head, объект Carey Barr]
-
+  console.log(getUsersWithAge(users, 20, 30));
   console.log(getUsersWithAge(users, 30, 40));
-  // [объект Moore Hensley, объект Sharlene Bush, объект Blackburn Dotson, объект Sheree Anthony]
 });

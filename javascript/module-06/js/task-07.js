7edaf9503c01035ee3d099e4bfecb19c33fa18f3
Dmitrij Shamota <su161084sd@gmail.com>
2020-08-09 14:50:32 +0300

    finish js-module-06-task7

--- javascript/module-06/js/task-07.js
+++ javascript/module-06/js/task-07.js
@@ -1,8 +1,7 @@
+import users from "./users.js"
 const buttonRef = document.querySelector('#task_07');
-const calculateTotalBalance = users => {
-  // твой код
-};
+const calculateTotalBalance = users => users.reduce((tatalBalance, user) => tatalBalance + user.balance, 0);
 
 buttonRef.addEventListener('click', () => {
-  console.log(calculateTotalBalance(users)); // 20916
+  console.log(calculateTotalBalance(users));
 });

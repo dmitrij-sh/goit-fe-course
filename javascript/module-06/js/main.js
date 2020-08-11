a2e0fb551296f18054548e45c7c89b82d282d3c3
Dmitrij Shamota <su161084sd@gmail.com>
2020-08-11 20:44:40 +0300

    fix js-module-06-task10

--- javascript/module-06/js/main.js
+++ javascript/module-06/js/main.js
@@ -87,14 +87,34 @@ buttonRef8.addEventListener('click', () => {
 
 // -----ЗАДАНИЕ 10-----//
 const buttonRef9 = document.querySelector('#task_10');
-const getSortedUniqueSkills = users =>
-  users.reduce((allSkills, user) => {
-    allSkills.push(...user.skills);
-    return allSkills
-      .filter((skill, i) => allSkills.indexOf(skill) === i)
-      .sort();
-  }, []);
+// const getSortedUniqueSkills = users =>
+//   users.reduce((allSkills, user) => {
+//     allSkills.push(...user.skills);
+//     return allSkills
+//       .filter((skill, i) => allSkills.indexOf(skill) === i)
+//       .sort();
+//   }, []);
 
+const getSortedUniqueSkills = users =>
+users.reduce((allSkills, user) => [...allSkills, ...user.skills], [])
+.filter((skill, i, arr) => arr.indexOf(skill) === i)
+.sort();
+
+// const getSortedUniqueSkills = function (users) {
+//   const dd = users
+//     .reduce(function (allSkills, user) {
+//       const bb = [...allSkills, ...user.skills];
+//       // console.log('acc --', allSkills, 'realArr---', user.skills);
+//       return bb
+//     }, [])
+//     .filter(function (skill, i, arr) {
+//       const zz = arr.indexOf(skill) === i;
+//       console.log('acc --', skill, 'iteration---', i, 'arr----', arr);
+//       return zz;
+//     })
+//     .sort();
+//   return dd;
+// };
 buttonRef9.addEventListener('click', () => {
   console.log(getSortedUniqueSkills(users));
 });

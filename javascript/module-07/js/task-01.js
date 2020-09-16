a1dc231fe46f65bda09cc8f64a2281c69bf7005d
Dmitrij Shamota <su161084sd@gmail.com>
2020-09-16 22:23:43 +0300

    finish js-module-07-task1

--- javascript/module-07/js/task-01.js
+++ javascript/module-07/js/task-01.js
@@ -1,3 +1,20 @@
 const buttonRef = document.querySelector('#task_01');
 
-buttonRef.addEventListener('click', () => {});
+buttonRef.addEventListener('click', () => {
+  const quantityItems = document.querySelectorAll('.item');
+  console.log(`В списке ${quantityItems.length} категории.`);
+
+  const titleSumOfItems = quantityItems.forEach(element =>
+    console.log(
+      `Категория: ${element.firstElementChild.textContent}, Количество элементов: ${element.lastElementChild.children.length}`,
+    ),
+  );
+
+  // SECOND METHOD
+  // const titleSumOfItems = quantityItems.forEach(element =>
+  //   console.log(
+  //     `Категория: ${element.querySelector('h2').textContent},
+  //      Количество элементов: ${element.querySelectorAll('li').length}`,
+  //   ),
+  // );
+});

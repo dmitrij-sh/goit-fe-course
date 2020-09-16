e2a40c2dab392180b14e08c05b58bc9d87dcf931
Dmitrij Shamota <su161084sd@gmail.com>
2020-09-16 23:24:24 +0300

    finish js-module-07-task2

--- javascript/module-07/js/task-02.js
+++ javascript/module-07/js/task-02.js
@@ -1,3 +1,23 @@
 const buttonRef = document.querySelector('#task_02');
+const ingredients = [
+  'Картошка',
+  'Грибы',
+  'Чеснок',
+  'Помидоры',
+  'Зелень',
+  'Приправы',
+];
 
-buttonRef.addEventListener('click', () => {});
+const listContainerRef = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const createListRef = document.createElement('li');
+  createListRef.textContent = ingredient;
+
+  return createListRef;
+});
+
+buttonRef.addEventListener('click', () => {
+  listContainerRef.append(...elements);
+  console.log(listContainerRef);
+});

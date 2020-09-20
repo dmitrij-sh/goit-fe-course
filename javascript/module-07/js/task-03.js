2b32baacc579bd09ad5abda5fc5cd3e46ef5dded
Dmitrij Shamota <su161084sd@gmail.com>
2020-09-20 09:11:58 +0300

    finish js-module-07-task3

--- javascript/module-07/js/task-03.js
+++ javascript/module-07/js/task-03.js
@@ -1,3 +1,35 @@
 const buttonRef = document.querySelector('#task_03');
 
-buttonRef.addEventListener('click', () => {});
+const images = [
+  {
+    url:
+      'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+];
+
+const listContainerRef = document.querySelector('#gallery');
+
+const createListMarkup = image => {
+  return `
+  <li class="task-section__item">
+    <img src="${image.url}", alt="${image.alt}">
+  </li>`;
+};
+
+const craeteGallery = images.map(image => createListMarkup(image)).join('');
+
+buttonRef.addEventListener('click', () => {
+  listContainerRef.insertAdjacentHTML('beforeend', craeteGallery);
+  console.log(listContainerRef);
+});

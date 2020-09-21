68f327e4f57a7df63e1f0252feabf49a0da062eb
Dmitrij Shamota <su161084sd@gmail.com>
2020-09-21 22:33:10 +0300

    finish js-module-07-task4

--- javascript/module-07/js/task-04.js
+++ javascript/module-07/js/task-04.js
@@ -1,3 +1,26 @@
 const buttonRef = document.querySelector('#task_04');
 
-buttonRef.addEventListener('click', () => {});
+const refs = {
+  value: document.querySelector('#value'),
+  buttonDec: document.querySelector('button[data-action="decrement"]'),
+  buttonInc: document.querySelector('button[data-action="increment"]'),
+};
+
+let counterValue = 0;
+
+function decrement() {
+  counterValue -= 1;
+  refs.value.textContent = counterValue;
+  // --refs.value.textContent;
+}
+
+function increment() {
+  counterValue += 1;
+  refs.value.textContent = counterValue;
+  // ++refs.value.textContent;
+}
+
+refs.buttonDec.addEventListener('click', decrement);
+refs.buttonInc.addEventListener('click', increment);
+
+// buttonRef.addEventListener('click', () => {});

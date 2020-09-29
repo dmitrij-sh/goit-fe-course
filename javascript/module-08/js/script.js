f17075213b3ea10817cafcf7e8bf413b0dec0643
Dmitrij Shamota <su161084sd@gmail.com>
2020-09-30 00:27:44 +0300

    add js-module-08 createGallery

--- javascript/module-08/js/script.js
+++ javascript/module-08/js/script.js
@@ -1,1 +1,44 @@
-import galleryItems from './gallery-items.js';
+import gallery from './gallery-items.js';
+
+const refs = {
+  galleryContainer: document.querySelector('.js-gallery'),
+};
+// const createListMarkupGallery = galleryItem => {
+//   `
+//   <li class="gallery__item">
+//   <a
+//     class="gallery__link"
+//     href="${galleryItem.original}"
+//   >
+//     <img
+//       class="gallery__image"
+//       src="${galleryItem.preview}"
+//       data-source="${galleryItem.original}"
+//       alt="${galleryItem.description}"
+//     />
+//   </a>
+// </li>`;
+
+//   const createGallery = galleryItems
+//     .map(galleryItem => createListMarkupGallery(galleryItem))
+//     .join('');
+
+//   return createGallery;
+// };
+// refs.galleryContainer.insertAdjacentHTML('beforeend', createGallery);
+// console.log();
+
+const createGallery = galleryItems => {
+  const createMarkup = galleryItems.map(
+    galleryItem =>
+      `<li class="gallery__item">
+      <a class="gallery__link" href="${galleryItem.original}">
+      <img class="gallery__image" src="${galleryItem.preview}"
+      data-source="${galleryItem.original}" alt="${galleryItem.description}"/>
+      </a></li>`,
+  );
+
+  refs.galleryContainer.innerHTML = createMarkup.join('');
+};
+
+createGallery(gallery);

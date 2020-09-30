1e973a713ac1c8e7857e381b77ba92284fc6d80b
Dmitrij Shamota <su161084sd@gmail.com>
2020-10-01 00:09:55 +0300

    add js-module-08 CloseModal

--- javascript/module-08/js/script.js
+++ javascript/module-08/js/script.js
@@ -1,44 +1,48 @@
-import gallery from './gallery-items.js';
+import gallerys from './gallery-items.js';
 
 const refs = {
   galleryContainer: document.querySelector('.js-gallery'),
-};
-// const createListMarkupGallery = galleryItem => {
-//   `
-//   <li class="gallery__item">
-//   <a
-//     class="gallery__link"
-//     href="${galleryItem.original}"
-//   >
-//     <img
-//       class="gallery__image"
-//       src="${galleryItem.preview}"
-//       data-source="${galleryItem.original}"
-//       alt="${galleryItem.description}"
-//     />
-//   </a>
-// </li>`;
-
-//   const createGallery = galleryItems
-//     .map(galleryItem => createListMarkupGallery(galleryItem))
-//     .join('');
-
-//   return createGallery;
-// };
-// refs.galleryContainer.insertAdjacentHTML('beforeend', createGallery);
-// console.log();
-
-const createGallery = galleryItems => {
-  const createMarkup = galleryItems.map(
-    galleryItem =>
-      `<li class="gallery__item">
-      <a class="gallery__link" href="${galleryItem.original}">
-      <img class="gallery__image" src="${galleryItem.preview}"
-      data-source="${galleryItem.original}" alt="${galleryItem.description}"/>
-      </a></li>`,
-  );
-
-  refs.galleryContainer.innerHTML = createMarkup.join('');
+  openModalLightbox: document.querySelector('.js-lightbox'),
+  closeModalBtn: document.querySelector('button[data-action="close-lightbox"]'),
+  lightboxImg: document.querySelector('.lightbox__image'),
 };
 
-createGallery(gallery);
+function createGalleryMarkup(gallerys) {
+  return gallerys
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}"
+      data-source="${original}" alt="${description}"/>
+      </a></li>`;
+    })
+    .join('');
+}
+
+refs.galleryContainer.insertAdjacentHTML(
+  'beforeend',
+  createGalleryMarkup(gallerys),
+);
+
+function onGalleryContainerClick(evt) {
+  evt.preventDefault();
+
+  onOpenModalClick();
+  changeAttributeLightboxImage(evt.target.dataset.source);
+}
+
+function onOpenModalClick(evt) {
+  refs.openModalLightbox.classList.add('is-open');
+}
+
+function changeAttributeLightboxImage(src) {
+  refs.lightboxImg.src = src;
+}
+
+function onCloseModalBtnClick(evt) {
+  refs.openModalLightbox.classList.remove('is-open');
+  refs.lightboxImg.src = '';
+}
+
+refs.galleryContainer.addEventListener('click', onGalleryContainerClick);
+refs.closeModalBtn.addEventListener('click', onCloseModalBtnClick);

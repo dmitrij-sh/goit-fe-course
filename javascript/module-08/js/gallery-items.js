de9fb08debfe9e90851982951993ab752a083ec2
Dmitrij Shamota <su161084sd@gmail.com>
2020-10-03 00:43:00 +0300

    add js-module-08 CloseModalEsc

--- javascript/module-08/js/gallery-items.js
+++ javascript/module-08/js/gallery-items.js
@@ -62,4 +62,88 @@ export default [
       'https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg',
     description: 'Lighthouse Coast Sea',
   },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2019/11/10/11/33/portrait-4615580__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2019/11/10/11/33/portrait-4615580_960_720.jpg',
+    description: 'Hapy Man',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/09/27/23/39/cow-5608144__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/09/27/23/39/cow-5608144_960_720.jpg',
+    description: 'Cow',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/06/14/17/51/dragonfly-5298749__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/06/14/17/51/dragonfly-5298749_960_720.jpg',
+    description: 'Dragonfly',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/06/23/12/00/flamingo-5332314__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/06/23/12/00/flamingo-5332314_960_720.jpg',
+    description: 'Flamingo',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/06/05/16/53/zucchini-5263781__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/06/05/16/53/zucchini-5263781_960_720.jpg',
+    description: 'Zucchini',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/06/20/10/40/spider-5320486__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/06/20/10/40/spider-5320486_960_720.jpg',
+    description: 'Spider',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/06/21/07/08/lichen-5323493__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/06/21/07/08/lichen-5323493_960_720.jpg',
+    description: 'Lichen',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/06/26/21/39/chicago-5344027__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/06/26/21/39/chicago-5344027_960_720.jpg',
+    description: 'Chicago',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/09/22/09/09/leaf-5592392__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/09/22/09/09/leaf-5592392_960_720.jpg',
+    description: 'Leaf',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/09/29/02/39/aircraft-5611528__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/09/29/02/39/aircraft-5611528_960_720.jpg',
+    description: 'Aircraft',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2016/11/06/17/17/north-america-1803504__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2016/11/06/17/17/north-america-1803504_960_720.jpg',
+    description: 'North America',
+  },
+  {
+    preview:
+      'https://cdn.pixabay.com/photo/2020/09/26/22/03/trees-5605176__340.jpg',
+    original:
+      'https://cdn.pixabay.com/photo/2020/09/26/22/03/trees-5605176_960_720.jpg',
+    description: 'Trees',
+  },
 ];
--- javascript/module-08/js/script.js
+++ javascript/module-08/js/script.js
@@ -5,6 +5,7 @@ const refs = {
   openModalLightbox: document.querySelector('.js-lightbox'),
   closeModalBtn: document.querySelector('button[data-action="close-lightbox"]'),
   lightboxImg: document.querySelector('.lightbox__image'),
+  backdrop: document.querySelector('.lightbox__overlay'),
 };
 
 function createGalleryMarkup(gallerys) {
@@ -12,8 +13,8 @@ function createGalleryMarkup(gallerys) {
     .map(({ preview, original, description }) => {
       return `<li class="gallery__item">
       <a class="gallery__link" href="${original}">
-      <img class="gallery__image" src="${preview}"
-      data-source="${original}" alt="${description}"/>
+      <img class="gallery__image" loading="lazy" src="${preview}"
+      data-source="${original}" alt="${description}" width="340" height="240"/>
       </a></li>`;
     })
     .join('');
@@ -27,11 +28,20 @@ refs.galleryContainer.insertAdjacentHTML(
 function onGalleryContainerClick(evt) {
   evt.preventDefault();
 
+  if (evt.target.nodeName !== 'IMG') {
+    return;
+  }
+
+  // if (!evt.target.classList.contains('gallery__image')) {
+  //   return;
+  // }
+
   onOpenModalClick();
   changeAttributeLightboxImage(evt.target.dataset.source);
 }
 
 function onOpenModalClick(evt) {
+  window.addEventListener('keydown', onCloseModalEscPress);
   refs.openModalLightbox.classList.add('is-open');
 }
 
@@ -40,9 +50,25 @@ function changeAttributeLightboxImage(src) {
 }
 
 function onCloseModalBtnClick(evt) {
+  window.removeEventListener('keydown', onCloseModalEscPress);
   refs.openModalLightbox.classList.remove('is-open');
   refs.lightboxImg.src = '';
 }
 
+function onCloseModalEscPress(evt) {
+  const ESC_KEY_CODE = 'Escape';
+
+  if (evt.code === ESC_KEY_CODE) {
+    onCloseModalBtnClick();
+  }
+}
+
+function onBackdropClick(evt) {
+  if (evt.currentTarget === evt.target) {
+    onCloseModalBtnClick();
+  }
+}
+
 refs.galleryContainer.addEventListener('click', onGalleryContainerClick);
 refs.closeModalBtn.addEventListener('click', onCloseModalBtnClick);
+refs.backdrop.addEventListener('click', onBackdropClick);

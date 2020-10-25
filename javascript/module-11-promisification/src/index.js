75d296bc852d35ff158f043a64c14d3421169c79
Dmitrij Shamota <su161084sd@gmail.com>
2020-10-25 14:43:39 +0200

    add markup js-module-11-promisification

---
+++ javascript/module-11-promisification/src/index.js
@@ -0,0 +1,5 @@
+import './css/reset.css';
+import './css/styles.css';
+import './js/delay';
+import './js/toggleUserState';
+import './js/makeTransaction';
---
+++ javascript/module-11-promisification/src/js/toggleUserState.js
@@ -0,0 +1,28 @@
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+const toggleUserState = (allUsers, userName, callback) => {
+  const updatedUsers = allUsers.map(user =>
+    user.name === userName ? { ...user, active: !user.active } : user,
+  );
+
+  callback(updatedUsers);
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
+toggleUserState(users, 'Mango', logger);
+toggleUserState(users, 'Lux', logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);

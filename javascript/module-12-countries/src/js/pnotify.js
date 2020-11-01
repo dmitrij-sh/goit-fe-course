65ceda84f620187f0d0ba31f8eabea25305563b7
Dmitrij Shamota <su161084sd@gmail.com>
2020-11-01 02:26:01 +0200

    finish  js-module-12-countries

---
+++ javascript/module-12-countries/src/js/pnotify.js
@@ -0,0 +1,4 @@
+import { error } from '@pnotify/core';
+import '@pnotify/core/dist/PNotify.css';
+import '@pnotify/core/dist/BrightTheme.css';
+export default error;
---
+++ javascript/module-12-countries/src/js/script.js
@@ -0,0 +1,60 @@
+const debounce = require('lodash.debounce');
+import error from './pnotify';
+import API from './fetchCountries';
+import countryCardTpl from '../templates/countryCard.hbs';
+import countryListTpl from '../templates/countryList.hbs';
+
+const refs = {
+  containerCard: document.querySelector('.country'),
+  input: document.querySelector('.form-input'),
+};
+
+refs.input.addEventListener('input', debounce(onSearchCountry, 500));
+
+function onSearchCountry(e) {
+  if (!e.target.value) {
+    clearRenderCard();
+    return;
+  }
+
+  API.fetchCountries(e.target.value)
+    .then(checkQuantityCountries)
+    .catch(fetchError);
+}
+
+function checkQuantityCountries(country) {
+  if (country.length > 10) {
+    error(
+      {
+        title: 'Too many matches found. Please enter a more specific query!',
+        delay: 2000,
+      },
+      200,
+    );
+    return;
+  }
+
+  if (country.length > 1 < 10) {
+    renderList(country);
+  }
+
+  if (country.length === 1) {
+    renderCard(country);
+  }
+}
+
+function renderCard(country) {
+  refs.containerCard.innerHTML = countryCardTpl(country);
+}
+
+function renderList(country) {
+  refs.containerCard.innerHTML = countryListTpl(country);
+}
+
+function clearRenderCard() {
+  refs.containerCard.innerHTML = '';
+}
+
+function fetchError(error) {
+  console.log(error);
+}

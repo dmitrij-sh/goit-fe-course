1e5107fdcac0c14531b331921ff7a624b6d00aaf
Dmitrij Shamota <su161084sd@gmail.com>
2020-11-08 23:03:05 +0200

    add function  js-module-13-image-finder

--- javascript/module-13-image-finder/src/js/script.js
+++ javascript/module-13-image-finder/src/js/script.js
@@ -3,6 +3,7 @@ import API from './apiService';
 import LoadMoreBtn from './loadMoreBtn';
 import onOpenModal from './openModal';
 import imageCardTpl from '../templates/imageCard.hbs';
+import animateScrollTo from 'animated-scroll-to';
 
 const imgApiService = new API();
 const loadMoreBtn = new LoadMoreBtn({
@@ -13,11 +14,14 @@ const loadMoreBtn = new LoadMoreBtn({
 const refs = {
   containerCard: document.querySelector('.gallery'),
   form: document.querySelector('.search-form'),
+  all: document.querySelectorAll('li'),
+  scrollBackBtn: document.querySelector('.scroll-up'),
 };
 
 refs.form.addEventListener('submit', onSearchImage);
 loadMoreBtn.refs.button.addEventListener('click', fetchImages);
 refs.containerCard.addEventListener('click', onOpenModal);
+window.addEventListener('scroll', scrollToBack);
 
 function onSearchImage(e) {
   e.preventDefault();
@@ -39,22 +43,45 @@ function fetchImages() {
   loadMoreBtn.disable();
   imgApiService.fetchImages().then(response => {
     renderImagesCard(response);
+    scrollToElement();
     success({
       text: 'Success',
       delay: 2000,
     });
+
     loadMoreBtn.enable();
   });
 }
 
-function renderImagesCard(img) {
-  refs.containerCard.insertAdjacentHTML('beforeend', imageCardTpl(img));
+function renderImagesCard(elements) {
+  refs.containerCard.insertAdjacentHTML('beforeend', imageCardTpl(elements));
 }
 
 function clearRenderCard() {
   refs.containerCard.innerHTML = '';
 }
 
-function scrollElement() {
-  scrollTo({ top: 1000, behavior: 'smooth' });
+function scrollToElement() {
+  const valueLength = refs.containerCard.children.length - 12;
+
+  animateScrollTo(refs.containerCard.children[`${valueLength}`], {
+    speed: 500,
+    maxDuration: 3000,
+    verticalOffset: -20,
+  });
+}
+
+function scrollToBack() {
+  const getTop = document.documentElement.scrollTop;
+  if (getTop > 180) {
+    refs.scrollBackBtn.classList.add('is-active');
+  } else {
+    refs.scrollBackBtn.classList.remove('is-active');
+  }
+  refs.scrollBackBtn.addEventListener('click', () => {
+    animateScrollTo(refs.form, {
+      speed: 500,
+      maxDuration: 3000,
+    });
+  });
 }
